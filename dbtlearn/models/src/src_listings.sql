cbca7636c28136ebbd2b422889d73b7f1f737b15
seemir <samir.adrik@gmail.com>
2023-10-15 09:29:31 +0200

    Created two test models for training

---
+++ dbtlearn/models/src/src_listings.sql
@@ -0,0 +1,14 @@
+with raw_listings as (
+    SELECT *
+    FROM AIRBNB.RAW.RAW_LISTINGS
+)
+SELECT ID    AS listings_id,
+       NAME  AS listings_name,
+       LISTING_URL,
+       ROOM_TYPE,
+       MINIMUM_NIGHTS,
+       HOST_ID,
+       PRICE AS price_str,
+       CREATED_AT,
+       UPDATED_AT
+FROM RAW_LISTINGS
