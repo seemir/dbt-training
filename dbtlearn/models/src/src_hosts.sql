cdfc493735835d74f25ec87a7a15bd731b4199a1
seemir <samir.adrik@gmail.com>
2023-10-16 21:09:45 +0200

    Added new model of host to dbt project

---
+++ dbtlearn/models/src/src_hosts.sql
@@ -0,0 +1,10 @@
+with raw_hosts as (
+    SELECT *
+    FROM AIRBNB.RAW.RAW_HOSTS
+)
+SELECT id   as host_id,
+       name as host_name,
+       is_superhost,
+       created_at,
+       updated_at
+FROM raw_hosts
